f84877a469251d11fc5ef7718532842760052504
Fangxing Liu <ge83toc@mytum.de>
2024-10-11 15:25:02 +0200

    Add Exercise List to display a List of all exercises and way to add new Exercises

--- Sport Tracker/Sport Tracker/Exercise Subsystem/Exercise Overview/AddExerciseView.swift
+++ Sport Tracker/Sport Tracker/Exercise Subsystem/Exercise Overview/AddExerciseView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct AddExerciseView: View{
+struct AddExerciseView: View {
     @Environment(Model.self) private var model: Model
     @Environment(\.dismiss) private var dismiss
-    
     @State var name: String = ""
     @State var description: String = ""
+    @State var exerciseTypes: [ExerciseType] = []
 
     var body: some View {
         NavigationStack {
@@ -20,14 +20,15 @@ struct AddExerciseView: View{
                 Section(header: Text("Name")) {
                     TextField("Name", text: $name)
                 }
-                Section(header: Text("Description")) {
-                    TextField("Description", text: $description)
+                Section(header: Text("Notes")) {
+                    TextField("", text: $description)
                 }
-            }.navigationBarTitle("Add Exercise", displayMode: .inline)
+                ExerciseTypePickerView(exerciseTypes: $exerciseTypes)
+            }.navigationTitle("Add Exercise")
                 .toolbar {
                     ToolbarItem(placement: .topBarTrailing) {
                         Button {
-                            model.addExercise(name: name, description: description)
+                            model.addExercise(name: name, description: description, exerciseTypes: exerciseTypes)
                             dismiss()
                         } label: {
                             Text("Add").bold()
@@ -38,7 +39,31 @@ struct AddExerciseView: View{
     }
 }
 
-
+struct ExerciseTypePickerView: View {
+    @Binding var exerciseTypes: [ExerciseType]
+    var exerciseTypeOverview: [ExerciseType] = [.repetitions, .weight, .duration]
+    var body: some View {
+        List {
+            ForEach(exerciseTypeOverview, id: \.self) { type in
+                Button(action: {
+                    exerciseTypes.contains(type)
+                    ? exerciseTypes.removeAll(where: { $0 == type })
+                    : exerciseTypes.append(type)
+                }, label: {
+                    HStack {
+                        Text(type.description)
+                        Spacer()
+                        if exerciseTypes.contains(type) {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(.green)
+                        }
+                    }
+                }
+                )
+            }
+        }
+    }
+}
 #Preview {
     AddExerciseView().environment(Model() as Model)
 }
