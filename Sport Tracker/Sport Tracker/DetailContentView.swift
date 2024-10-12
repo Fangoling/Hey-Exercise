b297e8b128c18c244514e3cc78dc73e05c944130
Fangxing Liu <ge83toc@mytum.de>
2024-10-12 18:26:49 +0200

    Add Main View with Tabview

--- Sport Tracker/Sport Tracker/DetailContentView.swift
+++ Sport Tracker/Sport Tracker/DetailContentView.swift
@@ -5,3 +5,20 @@
 //  Created by ipraktikum on 12.10.24.
 //
 
+import SwiftUI
+
+struct DetailContentView: View {
+    var body: some View {
+        TabView {
+            Tab("Workouts", systemImage: "list.bullet.indent") {
+                WorkoutOverviewView()
+            }
+            Tab("Exercises", systemImage: "dumbbell") {
+                ExerciseOverView()
+            }
+            Tab("Settings", systemImage: "gearshape") {
+                Settings()
+            }
+        }
+    }
+}
