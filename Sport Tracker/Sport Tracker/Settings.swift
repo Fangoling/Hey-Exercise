42a0fea17e66974a34e1024f04448ded72039924
Fangxing Liu <ge83toc@mytum.de>
2024-10-12 18:59:42 +0200

    add mock for notification settings

--- Sport Tracker/Sport Tracker/Settings.swift
+++ Sport Tracker/Sport Tracker/Settings.swift
@@ -5,3 +5,16 @@
 //  Created by ipraktikum on 12.10.24.
 //
 
+import SwiftUI
+
+struct Settings: View {
+    @Environment(Model.self) var model: Model
+    @State private var enabled = false
+    var body: some View {
+        Form {
+            Section {
+                Toggle("Show notifications", isOn: $enabled)
+            }
+        }
+    }
+}
